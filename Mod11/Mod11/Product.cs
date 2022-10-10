5891a1ff69aea0c5e3909d39d21d75954bebf2c5
UserMarcus <114788534+UserMarcus@users.noreply.github.com>
2022-10-10 16:21:15 +0800

    Add files via upload

---
+++ Mod11/Mod11/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod11
+{
+    class Product
+    {
+        public string name;
+        public int quantity;
+        public string unit;
+        public double price;
+        public int amount;
+
+        
+
+        public Product(string name, int quantity, string unit, double price, int amount)
+        {
+            this.name = name;
+            this.quantity = quantity;
+            this.unit = unit;
+            this.price = price;
+            this.amount = amount;
+        }
+    }
+}
---
+++ Mod11/Mod11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product = new Product("Ariel",2,"Sachet",7.50,2);
+            Console.WriteLine("Item: " + product.name);
+            Console.WriteLine("Quantity: " + product.quantity);
+            Console.WriteLine("Unit: " + product.unit);
+            Console.WriteLine("Price: " + product.price);
+            Console.WriteLine("Amount: " + product.amount);
+            Console.ReadLine();
+        }
+    }
+
+
+}
